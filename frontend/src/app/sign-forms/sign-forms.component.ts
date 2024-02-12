921bc92d9bfaf265d2bfd57995f3fabce99f3dcf
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-02-12 17:54:06 +0100

    feat: started forms FormGroup

--- frontend/src/app/sign-forms/sign-forms.component.ts
+++ frontend/src/app/sign-forms/sign-forms.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-forms',
   standalone: true,
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './sign-forms.component.html',
   styleUrl: './sign-forms.component.css'
 })
 export class SignFormsComponent {
+  signinForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  })
+
+  signupForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.minLength(4)),
+    confirmPassword: new FormControl('', Validators.minLength(4)),
+  })
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  onSubmitSignin() {
+
+  }
 
 }
