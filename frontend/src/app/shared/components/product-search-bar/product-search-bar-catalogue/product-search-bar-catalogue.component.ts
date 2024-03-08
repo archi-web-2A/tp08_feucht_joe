fedb9bfe01d9f111c522b98415ca880b7f48810f
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-03-08 20:29:34 +0100

    feat: products filtration

--- frontend/src/app/shared/components/product-search-bar/product-search-bar-catalogue/product-search-bar-catalogue.component.ts
+++ frontend/src/app/shared/components/product-search-bar/product-search-bar-catalogue/product-search-bar-catalogue.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from '@angular/core';
-import {ReactiveFormsModule} from "@angular/forms";
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-search-bar-catalogue',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    FormsModule
   ],
   templateUrl: './product-search-bar-catalogue.component.html',
   styleUrl: './product-search-bar-catalogue.component.css'
@@ -14,8 +15,15 @@ export class ProductSearchBarCatalogueComponent {
   @Input() placeholder: string
   @Input() inputIcon: string
 
+  @Output() inputChange = new EventEmitter<string>();
+  searchInputField!: string
+
   constructor() {
     this.placeholder = ''
     this.inputIcon = ''
+  }
+
+  onSearchInputChange(searchInputField: string) {
+    this.inputChange.emit(searchInputField.toLowerCase())
   }
 }
