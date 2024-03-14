5364daa117e6308883c78d105e2d4f75e6c2107b
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-03-14 14:50:09 +0100

    chore: updated tp title

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import {FooterComponent} from "./shared/components/footer/footer.component";
-import {HeroComponent} from "./features/hero/hero.component";
-import {ProductComponent} from "./features/product/product.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeroComponent } from './features/hero/hero.component';
+import { ProductComponent } from './features/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, ProductComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    ProductComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tp03_feucht_joe';
+  title = 'tp04_feucht_joe';
 }
--- frontend/src/app/shared/components/product-search-bar/product-search-bar-catalogue/product-search-bar-catalogue.component.ts
+++ frontend/src/app/shared/components/product-search-bar/product-search-bar-catalogue/product-search-bar-catalogue.component.ts
@@ -20,6 +20,4 @@ export class ProductSearchBarCatalogueComponent {
     this.placeholder = ''
     this.inputIcon = ''
   }
-
-
 }
